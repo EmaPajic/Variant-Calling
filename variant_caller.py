6e6048a01f3b1f499083672b4bcf4eaab8d7209e
doraaki <nikolaaleksic44@gmail.com>
2021-03-31 22:57:30 +0200

    Add option to call insertions and deletitions.

--- variant_caller.py
+++ variant_caller.py
@@ -4,38 +4,38 @@ class VariantCaller(object):
     def __init__(self):
         pass
 
-    def __calculate_most_probable_variant__(self, candidate_base_count, error_probability):
+    def __calculate_most_probable_variant__(self, candidate_variant_count, error_probability):
         """
-        Given two most common bases b and b', we want to calculate the likelihood of each possible variant
+        Given two most common variants v and v', we want to calculate the likelihood of each possible variant
         P(variant | reads) = P(reads | variant) * P(variant) / P(reads)
             => P(variant | reads) is proportional to P(reads | variant) if we ignore P(variant)
         
-        If we have k b' bases and n-k b bases, and the error probability for one read base of p:   
-            P(reads | variant is b) = C_nk * p^k * (1 - p)^(n-k)
-            P(reads | variant is b') = C_nk * p^(n - k) * (1 - p)^(n - k)
-            P(reads | variant is bb') = C_nk / 2^n 
+        If we have k v' bases and n-k v bases, and the error probability for one read position of p:   
+            P(reads | variant is v) = C_nk * p^k * (1 - p)^(n-k)
+            P(reads | variant is v') = C_nk * p^(n - k) * (1 - p)^(n - k)
+            P(reads | variant is vv') = C_nk / 2^n 
 
         As we have C_nk at all positions, we should just compare the rest. We can do that easier if we take the natural log:
-            ln(P(reads | variant is b)) is proportional to k * ln(p) + (n-k) * ln(1-p)
-            ln(P(reads | variant is b')) is proportional to (n-k) * ln(p) + k * ln(1-p)
-            ln(P(reads | variant is bb')) is proportional to n * ln(1/2)
+            ln(P(reads | variant is v)) is proportional to k * ln(p) + (n-k) * ln(1-p)
+            ln(P(reads | variant is v')) is proportional to (n-k) * ln(p) + k * ln(1-p)
+            ln(P(reads | variant is vv')) is proportional to n * ln(1/2)
         """
-        first_candidate_base, first_candidate_base_count = candidate_base_count[0]
-        second_candidate_base, second_candidate_base_count = candidate_base_count[1]
-        n = first_candidate_base_count + second_candidate_base_count
-        k = second_candidate_base_count
+        first_candidate_variant, first_candidate_variant_count = candidate_variant_count[0]
+        second_candidate_variant, second_candidate_variant_count = candidate_variant_count[1]
+        n = first_candidate_variant_count + second_candidate_variant_count
+        k = second_candidate_variant_count
 
-        first_base_log_likelihood = k * np.log(error_probability) + (n - k) * np.log(1 - error_probability)
-        second_base_log_likelihood = (n - k) * np.log(error_probability) + k * np.log(1 - error_probability)
+        first_variant_log_likelihood = k * np.log(error_probability) + (n - k) * np.log(1 - error_probability)
+        second_variant_log_likelihood = (n - k) * np.log(error_probability) + k * np.log(1 - error_probability)
         diploidy_log_likelihood = n * np.log(0.5)
-        total_likelihood = np.exp(first_base_log_likelihood) + np.exp(second_base_log_likelihood) + np.exp(diploidy_log_likelihood)
+        total_likelihood = np.exp(first_variant_log_likelihood) + np.exp(second_variant_log_likelihood) + np.exp(diploidy_log_likelihood)
 
-        if first_base_log_likelihood >= second_base_log_likelihood and first_base_log_likelihood >= diploidy_log_likelihood:
-            return ([first_candidate_base], np.exp(first_base_log_likelihood) / total_likelihood)
-        elif second_base_log_likelihood >= first_base_log_likelihood and second_base_log_likelihood >= diploidy_log_likelihood:
-            return ([second_candidate_base], np.exp(second_base_log_likelihood) / total_likelihood)
+        if first_variant_log_likelihood >= second_variant_log_likelihood and first_variant_log_likelihood >= diploidy_log_likelihood:
+            return ([first_candidate_variant], np.exp(first_variant_log_likelihood) / total_likelihood)
+        elif second_variant_log_likelihood >= first_variant_log_likelihood and second_variant_log_likelihood >= diploidy_log_likelihood:
+            return ([second_candidate_variant], np.exp(second_variant_log_likelihood) / total_likelihood)
         else:
-            return ([first_candidate_base, second_candidate_base], np.exp(diploidy_log_likelihood) / total_likelihood)
+            return ([first_candidate_variant, second_candidate_variant], np.exp(diploidy_log_likelihood) / total_likelihood)
         
 
     def call_variant(self, genomePositionInfo, error_probability = None):
@@ -43,13 +43,19 @@ class VariantCaller(object):
             # Deduce error probability from quality
             error_probability = 0.001
         
-        base_count = { base: genomePositionInfo[base] for base in {'A', 'G', 'C', 'T'} }
+        variant_count = { base: genomePositionInfo[base] for base in {'A', 'G', 'C', 'T'} }
+        if 'insertions' in genomePositionInfo:
+            for insertion_string, insertion_count in genomePositionInfo['insertions']:
+                variant_count[insertion_string] = insertion_count
+        if 'deletitions' in genomePositionInfo:
+            for deletition_string, deletition_string in genomePositionInfo['deletitions']:
+                variant_count[deletition_string] = deletition_count
         
         # Only keep two most likely bases
-        candidate_bases = sorted(base_count, key=base_count.get, reverse=True)[:2]
-        candidate_base_count = [ (base, base_count[base]) for base in candidate_bases ]
+        candidate_variants = sorted(variant_count, key=variant_count.get, reverse=True)[:2]
+        candidate_variant_count = [(variant, variant_count[variant]) for variant in candidate_variants]
 
-        most_probable_variant, confidence = self.__calculate_most_probable_variant__(candidate_base_count, error_probability)
+        most_probable_variant, confidence = self.__calculate_most_probable_variant__(candidate_variant_count, error_probability)
 
         genomePositionInfo['info'] = '.'
         genomePositionInfo['format'] = 'GT:VAF'
