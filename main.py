66c3a266a75d859927925bff37e4c1a580854d3c
doraaki <nikolaaleksic44@gmail.com>
2021-04-03 00:23:14 +0200

    Remove early stops in main.

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from pileup_reader import pileup_reader
 from variant_caller import VariantCaller
 from vcf_writer import create_vcf_file, write_vcf_line
+import time
 
 def main():
     variant_caller = VariantCaller()
     sample = 'SAMPLE1'
-    vcf = create_vcf_file('vcffile.txt', sample)
+    vcf = create_vcf_file('merged-normal.vcf', sample)
     
-    cnt = 0
+    start = time.time()
+
     for pileup_line in pileup_reader('merged-normal.pileup'):
         variant_caller.call_variant(pileup_line)
-        
         write_vcf_line(pileup_line, vcf, sample)
-
-        if pileup_line['alts'] != '.':
-            cnt += 1
-        if cnt == 100:
-            break
-
+    
+    print(time.time() - start)
 if __name__ == '__main__':
     main()
