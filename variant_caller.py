c83ffaffe6100b6347bc4dcaa428933429e3c001
doraaki <nikolaaleksic44@gmail.com>
2021-04-23 00:13:37 +0200

    Add comments about ref, alts and genotype writing.

--- variant_caller.py
+++ variant_caller.py
@@ -7,6 +7,18 @@ class VariantCaller(object):
 
     def __calculate_most_probable_variant__(self, candidate_variant_count, correct_probability):
         """
+        Parameters
+        ----------
+        candidate_variant_count: List of (str, int) tuples
+            List with two most common variants and their counts
+        correct_probability: float
+            Probability that one nucleotide in a read is correct
+        
+        Returns
+        -------
+        (List, float)
+        Chosen variants and estimated correctness probability
+
         Given two most common variants v and v', we want to calculate the likelihood of each possible variant
         P(variant | reads) = P(reads | variant) * P(variant) / P(reads)
             => P(variant | reads) is proportional to P(reads | variant) if we ignore P(variant)
@@ -37,9 +49,22 @@ class VariantCaller(object):
         
 
     def call_variant(self, genomePositionInfo, correct_probability = 0.8, use_read_quality = False):
+        """ Chooses variant and updates genomePositionInfo dictionary with chosen variant genotype, alts field, more
+
+        Parameters
+        ----------
+        genomePositionInfo: dictionary
+            All info about one pileup position
+        correct_probability: float
+            Probability that one nucleotide in a read is correct
+        use_read_quality: bool
+            Whether to use correct_probability estimate calculated using read qualities
+        """
         if use_read_quality:
             correct_probability = genomePositionInfo['average_quality']
         variant_count = { (base, 'SNV'): genomePositionInfo[base] for base in {'A', 'G', 'C', 'T'} }
+        
+        # Treat insertions and deletitions the same as SNVs
         if 'insertions' in genomePositionInfo:
             for insertion_string, insertion_count in genomePositionInfo['insertions']:
                 variant_count[(insertion_string, 'INS')] = insertion_count
@@ -67,7 +92,24 @@ class VariantCaller(object):
             most_probable_variant, confidence = self.__calculate_most_probable_variant__(candidate_variant_count, correct_probability)
 
         ref_variant_present = len([variant[0] for variant in most_probable_variant if (variant[0] == genomePositionInfo['ref_base'] and variant[1] == 'SNV')]) > 0
+
         alt_variants = [variant[0] for variant in most_probable_variant if variant[0] != genomePositionInfo['ref_base']]
+
+        """
+        After calling the variants, we need to store them in the vcf format.
+        That means determining the ref_base field, alts field and genotype.
+
+        |------------------------------------------------------------|
+        |                    Table for some SNVs                     |
+        |------------------------------------------------------------|
+        |original ref|called variants|ref in vcf|alts in vcf|genotype|
+        |------------------------------------------------------------|
+        |     G      |       G       |     G    |     .     |   0/0  |
+        |     G      |       T       |     G    |     T     |   1/1  |
+        |     G      |      G,T      |     G    |     T     |   0/1  |
+        |     G      |      A,T      |     G    |    A,T    |   1/2  |
+        --------------------------------------------------------------
+        """
 
         genomePositionInfo['vaf'] = confidence
         if len(alt_variants) == 0:
@@ -82,9 +124,25 @@ class VariantCaller(object):
             else:
                 genomePositionInfo['genotype'] = (1, 2)
 
-        # TODO: Add comments for INDEL ref and alts forming (everything below)
+        # Extract variant types to diversify INDELs from SNVs
         alt_variant_types = [variant[1] for variant in most_probable_variant if variant[0] != genomePositionInfo['ref_base']]
 
+        """
+        Insertions and deletitions require some extra work when writing to alt and ref_base field, 
+        given that we might want to change ref_base if we have deletitions, or store insertions as
+        ref_base + insertion. Some examples of INDEL storing in vcf:
+
+        |------------------------------------------------------------|
+        |                    Table for some INDELS                   |
+        |------------------------------------------------------------|
+        |original ref|called variants|ref in vcf|alts in vcf|genotype|
+        |------------------------------------------------------------|
+        |     G      |     CTC IN    |     G    |    GCTC   |   1/1  |
+        |     G      |     CTC DEL   |   GCTC   |     G     |   1/1  |
+        |     G      |AC DEL, ACA DEL|   GACA   |    GA,G   |   1/2  |
+        |     G      | TC DEL, AC IN |   GTC    |  G,GTCAC  |   1/2  |
+        --------------------------------------------------------------
+        """
         for position in range(len(alt_variants)):
             if alt_variant_types[position] == 'INS':
                 alt_variants[position] = genomePositionInfo['ref_base'] + alt_variants[position]
